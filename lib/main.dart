912a26414c5fb17cc1318cb184aed72b9c2201df
TitusKI <kidumazie@gmail.com>
2024-04-11 13:16:27 +0200

    Refactor and add Global

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:cineflix/firebase_options.dart';
+import 'package:cineflix/global.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
@@ -10,7 +9,6 @@ import 'src/app.dart';
 // }
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Global.init();
   runApp(const App());
 }

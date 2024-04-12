e228a6ec393259a14a669de346060024cf7b45a3
TitusKI <kidumazie@gmail.com>
2024-04-12 11:28:41 +0200

    Trying to add LoadingPage

--- lib/src/pages/register/bloc/register_state.dart
+++ lib/src/pages/register/bloc/register_state.dart
@@ -5,7 +5,10 @@ class RegisterStates {
   final String repassword;
 
   const RegisterStates(
-      {this.userName = "", this.email  = "", this.password  = "", this.repassword  = ""});
+      {this.userName = "",
+      this.email = "",
+      this.password = "",
+      this.repassword = ""});
 
   RegisterStates copyWith({
     String? userName,
@@ -13,7 +16,27 @@ class RegisterStates {
     String? password,
     String? repassword,
   }) {
-    return RegisterStates(userName: userName ?? this.userName, email: email ?? this.email,
-        password:password??this.password,  repassword :repassword ?? this.repassword);
+    return RegisterStates(
+        userName: userName ?? this.userName,
+        email: email ?? this.email,
+        password: password ?? this.password,
+        repassword: repassword ?? this.repassword);
   }
+}
+
+class RegisterSuccessState extends RegisterStates {
+  final bool isRegisterSuccess;
+  const RegisterSuccessState({this.isRegisterSuccess = false});
+}
+
+class RegisterLoadingState extends RegisterStates {
+  final bool isRegisterLoading;
+  const RegisterLoadingState({this.isRegisterLoading = false});
+}
+
+class RegisterFailurState extends RegisterStates {
+  final String error;
+  const RegisterFailurState(this.error);
+  @override
+  List<Object> get props => [error];
 }
--- lib/src/pages/register/register.dart
+++ lib/src/pages/register/register.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 import "package:flutter_screenutil/flutter_screenutil.dart";
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 class Register extends StatefulWidget {
   const Register({super.key});
 
@@ -19,70 +18,77 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<RegisterBloc, RegisterStates> (
-      builder: (context, state){
+    return BlocBuilder<RegisterBloc, RegisterStates>(
+      builder: (context, state) {
         return Container(
-      color: AppColors.primaryBackground,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: AppColors.primarySecondaryBackground,
-          appBar: buildAppBar("Sign Up"),
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 20.h,
+          color: AppColors.primaryBackground,
+          child: SafeArea(
+            child: Scaffold(
+              backgroundColor: AppColors.primarySecondaryBackground,
+              appBar: buildAppBar("Sign Up"),
+              body: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Center(
+                        child: reusableText(
+                            "Enter your details below and free sign Up")),
+                    Container(
+                      padding: EdgeInsets.only(left: 25.w, right: 25.w),
+                      margin: EdgeInsets.only(top: 50.h),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          reusableText("User name"),
+                          buildTextField("Enter your user name", "name", "user",
+                              (value) {
+                            context
+                                .read<RegisterBloc>()
+                                .add(UserNameEvent(value));
+                          }),
+                          reusableText("Email"),
+                          buildTextField(
+                              "Enter your email address", "email", "user",
+                              (value) {
+                            context.read<RegisterBloc>().add(EmailEvent(value));
+                          }),
+                          reusableText("Password"),
+                          buildTextField(
+                              "Enter your password", "password", "lock",
+                              (value) {
+                            context
+                                .read<RegisterBloc>()
+                                .add(PasswordEvent(value));
+                          }),
+                          reusableText("Confirm Password"),
+                          buildTextField("Re-enter your password to confirm",
+                              "confirmpassword", "lock", (value) {
+                            context
+                                .read<RegisterBloc>()
+                                .add(RepasswordEvent(value));
+                          }),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 25.w),
+                      child: reusableText(
+                          "By creating an account you have agree with our terms and conditions"),
+                    ),
+                    buildLogInAndRegButton("Sign Up", "login", () {
+                      // Navigator.of(context).pushNamed("register");
+                      RegisterController(context).handleEmailRegister();
+                    })
+                  ],
                 ),
-                Center(
-                    child: reusableText(
-                        "Enter your details below and free sign Up")),
-                Container(
-                  padding: EdgeInsets.only(left: 25.w, right: 25.w),
-                  margin: EdgeInsets.only(top: 50.h),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      reusableText("User name"),
-                      buildTextField("Enter your user name", "name", "user",
-                          (value) {
-                        context.read<RegisterBloc>().add(UserNameEvent(value));
-                      }),
-                      reusableText("Email"),
-                      buildTextField(
-                          "Enter your email address", "email", "user", (value) {
-                        context.read<RegisterBloc>().add(EmailEvent(value));
-                      }),
-                      reusableText("Password"),
-                      buildTextField("Enter your password", "password", "lock",
-                          (value) {
-                        context.read<RegisterBloc>().add(PasswordEvent(value));
-                      }),
-                      reusableText("Confirm Password"),
-                      buildTextField("Re-enter your password to confirm",
-                          "confirmpassword", "lock", (value) {
-                        context.read<RegisterBloc>().add(RepasswordEvent(value));
-                      }),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 25.w),
-                  child: reusableText(
-                      "By creating an account you have agree with our terms and conditions"),
-                ),
-                buildLogInAndRegButton("Sign Up", "login", () {
-                  // Navigator.of(context).pushNamed("register");
-                   RegisterController(context).handleEmailRegister();
-                })
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
-  
+        );
       },
-      );
+    );
   }
 }

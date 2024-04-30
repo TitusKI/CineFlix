114bc3172196871409b5a4ae0ff023689717ee82
tsegaye27 <ttalegn29@gmail.com>
2024-04-30 15:32:19 +0300

    fixing the missing key parameter

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -5,8 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }

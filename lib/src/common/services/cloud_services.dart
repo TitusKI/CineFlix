f972a50d7570009299aaaf48fc5e7c86e07ca951
TitusKI <kidumazie@gmail.com>
2024-04-30 21:34:31 +0200

    Logic Behind favorite movie list on firebase

--- lib/src/common/services/cloud_services.dart
+++ lib/src/common/services/cloud_services.dart
@@ -3,44 +3,52 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FavoriteServices {
-  final CollectionReference moviesCollection =
-      FirebaseFirestore.instance.collection("favorite");
-  ItemModel? itemModel;
+  final String? _userEmail = FirebaseAuth.instance.currentUser!.email;
+  final CollectionReference myCollection =
+      FirebaseFirestore.instance.collection('UsersWithFavorite');
+  // // final DocumentReference myCollectionDoc = myCollection.doc(_userEmail);
+  // final CollectionReference moviesCollection = FirebaseFireStore.instance.collection("favorite");
+  // ItemModel? itemModel;
 
-  Future<void> addFavorite(ItemModel itemModel, int index) async {
-    var movie = itemModel.results[index];
-    String? userEmail = FirebaseAuth.instance.currentUser?.email;
-    if (userEmail != null) {
-      try {
-        await moviesCollection.doc(movie.id.toString()).set({
-          'userEmail': userEmail,
-          'imageUrl': movie.poster_path,
-          'title': movie.title,
-          'date': movie.release_date,
-          'rating': movie.vote_average,
-          'description': movie.overview,
-          'timestamp': Timestamp.now(),
-        });
-      } catch (e) {
-        print(e.toString());
-      }
+  Future<void> addFavorite(ItemModel? itemModel, int index) async {
+    var moviesModel = itemModel!.results[index];
+    try {
+      await myCollection
+          .doc(_userEmail)
+          .collection('favorite')
+          .doc(moviesModel.id.toString())
+          .set({
+        'imageUrl': moviesModel.poster_path,
+        'title': moviesModel.title,
+        'date': moviesModel.release_date,
+        'rating': moviesModel.vote_average,
+        'description': moviesModel.overview,
+        'timestamp': Timestamp.now(),
+      });
+    } catch (e) {
+      print(e.toString());
     }
   }
 
   Future<void> deleteFavorite(int? id) {
-    return moviesCollection.doc(id.toString()).delete();
+    return myCollection
+        .doc(_userEmail)
+        .collection('favorite')
+        .doc(id.toString())
+        .delete();
   }
 
-  Stream<QuerySnapshot> getRetriveData(String? userEmail) {
-    if (userEmail != null) {
-      final favoriteStream = FirebaseFirestore.instance
-          .collection('movies')
-          .where('userEmail', isEqualTo: userEmail)
-          .snapshots();
-      return favoriteStream;
-    } else {
-      // Return an empty stream if userEmail is null
-      return const Stream.empty();
-    }
+  Stream<QuerySnapshot> getRetriveData(String? email) {
+    // if (email == myCollection.doc(_userEmail).toString()) {
+    final favoriteStream = myCollection
+        .doc(email)
+        .collection('favortie')
+        .orderBy('timestamp', descending: true)
+        .snapshots();
+    print("favorite Data list : $favoriteStream");
+    return favoriteStream;
+    // } else {
+    //   return const Stream.empty();
+    // }
   }
 }

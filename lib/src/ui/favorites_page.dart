9c13f7edd287f0d53592abeb65d987fdafdd8663
Kaleab Solomon <kaleabslmn@gmail.com>
2024-05-01 21:39:47 +0300

    fix data overflow from the ui fo favorites page

--- lib/src/ui/favorites_page.dart
+++ lib/src/ui/favorites_page.dart
@@ -1,17 +1,15 @@
-import 'package:cineflix/src/blocs/movies_detail_bloc_provider.dart';
 import 'package:cineflix/src/common/services/cloud_services.dart';
 import 'package:cineflix/src/common/values/colors.dart';
-import 'package:cineflix/src/models/item_model.dart';
 import 'package:cineflix/src/models/people_model.dart';
 import 'package:cineflix/src/pages/common_widgets.dart';
 import 'package:cineflix/src/resources/people_api_provider.dart';
 import 'package:cineflix/src/ui/item_navigation.dart';
-import 'package:cineflix/src/ui/movie_detail.dart';
 
 import 'package:cineflix/src/ui/widgets/star_rating.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -75,72 +73,73 @@ class _FavoritesPageState extends State<FavoritesPage> {
           print('rating: $rating');
           String description = data['description'] ?? " ";
           print('description: $description');
-          return Column(
-            children: [
-              ListTile(
-                onTap: () async {
-                  print('All snapshots are : $snapshot');
-                  cast = await pplApi.fetchPeople(
-                      snapshot.data!.results[index].id, 2);
-                  print('All Data : ${snapshot.data!.results[index]}');
-                  openDetailPage(
-                    context,
-                    snapshot.data,
-                    cast,
-                    index,
-                  );
-                },
-
-                //   );
-                // },
-                title: AspectRatio(
-                  aspectRatio: 2 / 3,
-                  child: Image(
-                    alignment: Alignment.bottomCenter,
-                    fit: BoxFit.contain,
-                    image: NetworkImage(
-                      'https://image.tmdb.org/t/p/w500$imageUrl',
-                      // scale: 5.0,
-                    ),
-                  ),
-                ),
-                trailing: Container(
-                  padding: const EdgeInsets.all(4.0),
-                  alignment: Alignment.topCenter,
-                  width: 150,
-                  height: 400, // Adjust the width as needed
-                  child: Wrap(
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisSize: MainAxisSize.min, // Use min instead of max
-                    children: [
-                      Text(
-                        title,
-                        textAlign: TextAlign.justify,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+          return GestureDetector(
+            onTap: () async {
+              print('All snapshots are : $snapshot');
+              cast =
+                  await pplApi.fetchPeople(snapshot.data!.results[index].id, 2);
+              print('All Data : ${snapshot.data!.results[index]}');
+              openDetailPage(
+                context,
+                snapshot.data,
+                cast,
+                index,
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 5),
+              color: Colors.black,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: AspectRatio(
+                      aspectRatio: 2 / 3,
+                      child: Image(
+                        alignment: Alignment.bottomCenter,
+                        fit: BoxFit.contain,
+                        image: NetworkImage(
+                          'https://image.tmdb.org/t/p/w500$imageUrl',
+                          // scale: 5.0,
                         ),
                       ),
-                      reusableText(date),
-                      StarRating(voteAverage: rating),
-                      Text(
-                        maxLines: 5,
-                        description,
-                        style: const TextStyle(
-                            fontSize: 14,
-                            textBaseline: TextBaseline.ideographic,
-                            overflow: TextOverflow.ellipsis),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    padding: const EdgeInsets.all(4.0),
+                    alignment: Alignment.topCenter,
+                    width: 200,
+                    // height: 300, // Adjust the width as needed
+                    child: Wrap(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisSize: MainAxisSize.min, // Use min instead of max
+                      children: [
+                        Text(
+                          title,
+                          textAlign: TextAlign.justify,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        reusableText(date),
+                        StarRating(voteAverage: rating),
+                        Text(
+                          maxLines: 5,
+                          description,
+                          style: const TextStyle(
+                              fontSize: 14,
+                              textBaseline: TextBaseline.ideographic,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              )
-            ],
+            ),
           );
         });
   }
@@ -152,6 +151,7 @@ class _FavoritesPageState extends State<FavoritesPage> {
             .getRetriveData(FirebaseAuth.instance.currentUser?.email),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            print("Built the snapshot");
             return buildFavoriteList(snapshot);
           } else if (snapshot.connectionState == ConnectionState.none ||
               snapshot.connectionState == ConnectionState.waiting) {
